30ad6c46bd027fd73ab9c2d58885d949ec7c8169
Wei Fu <37407014+KellaDoe@users.noreply.github.com>
2025-03-19 16:47:59 +0800

    Update dataloader.py

--- Data/dataloader.py
+++ Data/dataloader.py
@@ -16,27 +16,11 @@ def data_class(dataset,test_all=0):
     if test_all:
         if dataset == 'isic': return 8
         elif dataset == 'bone': return 21
-        elif dataset == 'retinal': return 8
-        elif dataset == 'limuc': return 4
-        elif dataset == 'dra': return 5
-        elif dataset == 'pancreas_old': return 9
-        elif dataset == 'pancreas128': return 8
-        elif dataset == 'pancreas256': return 8
-        elif dataset == 'pancreas256m': return 8
-        elif dataset == 'pancreas_pdacv': return 8
         else:
             raise NotImplementedError('dataset not found')
     else:
         if dataset == 'isic': return 6
         elif dataset == 'bone': return 10
-        elif dataset == 'retinal': return 7
-        elif dataset == 'limuc': return 3
-        elif dataset == 'dra': return 4
-        elif dataset == 'pancreas_old': return 3
-        elif dataset == 'pancreas128': return 6
-        elif dataset == 'pancreas256': return 3
-        elif dataset == 'pancreas256m': return 3
-        elif dataset == 'pancreas_pdacv': return 7
         else: raise NotImplementedError('dataset not found')
 
 def dataloader(dataset = 'isic', test_all=0, data_mode = 'train', batch_size = 32, num_workers = 8,
@@ -67,30 +51,6 @@ def dataloader(dataset = 'isic', test_all=0, data_mode = 'train', batch_size = 3
     elif dataset == 'bone':
         from Data.bone import BoneDataset
         valid = BoneDataset(transform=transform,test_all=test_all,data_mode=data_mode)
-    elif dataset == 'retinal':
-        from Data.retinal import RetinalDataset
-        valid = RetinalDataset(transform=transform,test_all=test_all,data_mode=data_mode)
-    elif dataset == 'limuc':
-        from Data.limuc import LIMUCDataset
-        valid = LIMUCDataset(transform=transform,test_all=test_all,data_mode=data_mode)
-    elif dataset == 'dra':
-        from Data.dra import DRADataset
-        valid = DRADataset(transform=transform,test_all=test_all,data_mode=data_mode)
-    elif dataset == 'pancreas_old':
-        from Data.pancreas_old import PancreasDataset
-        valid = PancreasDataset(transform=transform,test_all=test_all,data_mode=data_mode)
-    elif dataset == 'pancreas128':
-        from Data.pancreas_128 import PancreasDataset
-        valid = PancreasDataset(transform=transform,test_all=test_all,data_mode=data_mode)
-    elif dataset == 'pancreas256':
-        from Data.pancreas_256 import PancreasDataset
-        valid = PancreasDataset(transform=transform,test_all=test_all,data_mode=data_mode)
-    elif dataset == 'pancreas256m':
-        from Data.pancreas_256m import PancreasDataset
-        valid = PancreasDataset(transform=transform,test_all=test_all,data_mode=data_mode)
-    elif dataset == 'pancreas_pdacv':
-        from Data.pancreas_pdacv import PancreasDataset
-        valid = PancreasDataset(transform=transform,test_all=test_all,data_mode=data_mode)
     else:
         raise NotImplementedError('dataset not found')
     
